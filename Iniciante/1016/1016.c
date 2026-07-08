262bac38ee2318cbe8b65f75d9e51ef04eb1a390
Guilherme <glhmartinsrangel@gmail.com>
2026-07-08 13:23:49 -0300

    exercicio 1016 em c, c++, python e java

---
+++ Iniciante/1016/1016.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int km, calculo;
+    scanf("%d", &km);
+    calculo = km*2;
+    printf("%d minutos\n", calculo);
+    return 0;
+}
---
+++ Iniciante/1016/1016.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner i = new Scanner(System.in);
+        int x = i.nextInt();
+        System.out.println(2*x + " minutos");
+    }
+}
